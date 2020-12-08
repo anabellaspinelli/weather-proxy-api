93290b3c02f12a6af1f98eff0d99327fa5cd4339
Anabella Spinelli <anabella.spinelli@typeform.com>
2020-12-08 18:14:26 -0300

    feat: add /weather handler

---
+++ src/config.ts
@@ -0,0 +1,3 @@
+export const WEATHER_API_KEY = process.env.WEATHER_API_KEY
+export const WEATHER_API_URL =
+    'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/weatherdata'
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
-import { Request, Response } from 'express'
-import express from 'express'
 import * as dotenv from 'dotenv'
+import express, { Request, Response } from 'express'
+import fetch from 'node-fetch'
+import { URL, URLSearchParams } from 'url'
+import { WEATHER_API_URL } from './config'
 dotenv.config()
 
 const app = express()
@@ -8,6 +10,26 @@ const port = 3001
 
 app.get('/_health', (_: Request, res: Response) => {
     res.send(`up and running on port ${port}`)
+})
+
+app.get('/weather', async (req: Request, res: Response) => {
+    const url = new URL(`${WEATHER_API_URL}/history`)
+    const params = {
+        aggregateHours: '24',
+        locationMode: 'single',
+        period: req.query.period,
+        contentType: 'json',
+        unitGroup: 'metric',
+        // eslint-disable-next-line no-undef
+        key: process.env.WEATHER_API_KEY,
+        locations: req.query.location,
+    }
+
+    url.search = new URLSearchParams(params as any).toString()
+
+    const weatherResponse = await fetch(url)
+    const weatherBody = await weatherResponse.json()
+    res.send(weatherBody)
 })
 
 app.listen(port, () => {
