8524b08abaf69f244f86861235158499da079a05
Alba Rincón <alba.rincon@typeform.com>
2021-02-21 18:27:58 +0100

    Added new /weather-comparison endpoint

--- src/types.ts
+++ src/types.ts
@@ -2,3 +2,18 @@ export type HistoryQuery = {
     period: string
     location: string
 }
+
+export type TimelineQuery = {
+    date: string
+    location: string
+}
+
+export type WeatherComparison = {
+    address: string
+    days: Array<Day>
+}
+
+export type Day = {
+    datetime: string
+    temp: number
+}
