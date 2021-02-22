4fb62a557f47de59b723b2cafa54d97f81290681
Anabella Spinelli <anabellaspinelli@gmail.com>
2021-02-22 22:25:17 +0100

    added new relic dependency

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import './load-env'
 import app from './app'
 import { PORT } from './config'
 
+require('newrelic')
+
 app.listen(PORT, () => {
     // tslint:disable-next-line:no-console
     console.info(`Listening on port ${PORT}`)
